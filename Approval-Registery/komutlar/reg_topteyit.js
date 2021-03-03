b991964baf993f253bcbf05e31465a9f1deb5dbf
Memo Approval <74969246+Approval-cyber@users.noreply.github.com>
2021-03-03 12:28:08 +0300

    Update reg_topteyit.js

--- Approval-Registery/komutlar/reg_topteyit.js
+++ Approval-Registery/komutlar/reg_topteyit.js
@@ -18,7 +18,7 @@ exports.run = async (client, message, args) => {
   
     let teyitData = rdb.get("reg") || {};
     let data = Object.keys(teyitData);
-    let dataTop = data.filter(x => message.guild.members.cache.has(x)).sort((a, b) => Number((teyitData[b].erkek || 0) + (teyitData[b].kadin || 0)) - Number((teyitData[a].erkek) + (teyitData[a].kadin))).map((value, index) => `\`${index+1}.\` ${message.guild.members.cache.get(value)} **\`${teyitData[value].erkek || 0}\` erkek, \`${teyitData[value].kadin || 0}\` kadın [${(teyitData[value].erkek || 0) + (teyitData[value].kadin || 0)}]**`).splice(0, 15).join("\n");
+    let dataTop = data.filter(x => message.guild.members.cache.has(x)).sort((a, b) => Number((teyitData[b].erkek || 0) + (teyitData[b].kız || 0)) - Number((teyitData[a].erkek) + (teyitData[a].kız))).map((value, index) => `\`${index+1}.\` ${message.guild.members.cache.get(value)} **\`${teyitData[value].erkek || 0}\` erkek, \`${teyitData[value].kız || 0}\` kadın [${(teyitData[value].erkek || 0) + (teyitData[value].kız || 0)}]**`).splice(0, 15).join("\n");
     message.channel.send(approvemb.setTitle('Top Teyit').setDescription(`${dataTop || 'Verileri köpeğim yedi...'}`));
 
  
